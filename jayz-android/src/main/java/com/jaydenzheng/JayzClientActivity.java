458ccdade75255efcf331d65d0635717c5b8e780
unknown <superhck@yahoo.com>
2015-09-18 19:11:39 -0400

    Add jayz-android project.

---
+++ jayz-android/src/main/java/com/jaydenzheng/JayzClientActivity.java
@@ -0,0 +1,14 @@
+package com.jaydenzheng;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Rong Zheng on 9/18/2015.
+ */
+public class JayzClientActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
